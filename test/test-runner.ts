40b7230ea4b98cb49e24355778917aa0b3f7c220
Ruslan <rusishss@mail.ru>
2023-05-24 21:55:04 +0600

    created e2e tests for post likes operations

--- test/test-runner.ts
+++ test/test-runner.ts
@@ -2,9 +2,9 @@ import { testBlogCrud } from './01-blogs.e2e-spec';
 import { testPostCrud } from './02-posts.e2e-spec';
 import { testCommentsCrud } from './04-comments.e2e-spec';
 import { testBloggerCrud } from './07-blogger.blogs.controller';
-import { testCommentsAndLikesCrud } from './08-comments-and-likes.operation.e2e-spec';
+import { testPostLikesCrud } from './08-post-likes.operation.e2e-spec';
 
 describe('End-to-End Tests', () => {
-  //testBloggerCrud()
-  testCommentsAndLikesCrud()
+  testBloggerCrud()
+  testPostLikesCrud()
 });
